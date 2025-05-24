77ed4ba45521f73e3e15960843d0f0679b3bc40d
Alex Bruno <Bssallex@gmail.com>
2025-05-24 20:20:48 -0300

    feat: Cria a camada de entidade na parte externa da aplicação

---
+++ src/main/java/MovieRating/infrastructure/persistence/MovieRatingEntity.java
@@ -0,0 +1,39 @@
+package MovieRating.infrastructure.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MovieRatingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String movieTitle;
+
+    String description;
+
+    String genre;
+
+    String review;
+
+    String userExperience;
+
+    BigDecimal rating;
+
+    LocalDate releaseYear;
+
+    LocalDate dateWatched;
+}
