8c4a673281e1961f90aa832c0c695d30ee57c7ea
Alex Bruno <Bssallex@gmail.com>
2025-05-24 20:22:39 -0300

    feat: Cria a camada de repository que extende o JPA

---
+++ src/main/java/MovieRating/infrastructure/persistence/MovieRepository.java
@@ -0,0 +1,8 @@
+package MovieRating.infrastructure.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<MovieRatingEntity, Long> {
+}
