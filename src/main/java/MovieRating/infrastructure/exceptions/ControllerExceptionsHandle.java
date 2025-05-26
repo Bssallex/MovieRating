18b8908c0960eefb8b29e4bc221e3e33cc52c9ea
Alex Bruno <Bssallex@gmail.com>
2025-05-26 17:42:09 -0300

    feat: Cria uma classe para gerenciar o tratamento de exceptions personalizadas

---
+++ src/main/java/MovieRating/infrastructure/exceptions/ControllerExceptionsHandle.java
@@ -0,0 +1,24 @@
+package MovieRating.infrastructure.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerExceptionsHandle {
+
+    @ExceptionHandler(NotFoundExceptions.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(NotFoundExceptions notFoundExceptions){
+
+        Map<String, String> exceptions = new HashMap<>();
+        exceptions.put("Error: ", notFoundExceptions.getMessage());
+        exceptions.put("Message: ", "The request event was not found, check the identifier try again");
+        return new ResponseEntity<>(exceptions, HttpStatus.CONFLICT);
+    }
+
+
+}
