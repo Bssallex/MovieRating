47c49d610bd124867e7de9b1bad1a17b1d0456be
Alex Bruno <Bssallex@gmail.com>
2025-05-26 17:41:01 -0300

    feat: Cria uma exception personalizada: Nenhum objeto encontrado

---
+++ src/main/java/MovieRating/infrastructure/exceptions/NotFoundExceptions.java
@@ -0,0 +1,8 @@
+package MovieRating.infrastructure.exceptions;
+
+public class NotFoundExceptions extends RuntimeException{
+
+    public NotFoundExceptions(String message){
+        super(message);
+    }
+}
