e2db378424a2834cd4276b5f0a3fddf7ed366a1d
Alex Bruno <Bssallex@gmail.com>
2025-05-26 14:20:31 -0300

    feat: Cria os endpoints de listar e salvar

--- src/main/java/MovieRating/infrastructure/presentation/MovieRatingController.java
+++ src/main/java/MovieRating/infrastructure/presentation/MovieRatingController.java
@@ -1,11 +1,37 @@
 package MovieRating.infrastructure.presentation;
 
+import MovieRating.core.entity.MovieRating;
+import MovieRating.core.usecases.MovieFindAllUseCase;
+import MovieRating.core.usecases.MovieSaveUseCase;
+import MovieRating.infrastructure.dtos.MovieRatingDto;
+import MovieRating.infrastructure.mapper.MovieRatingMapping;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class MovieRatingController {
+
+    private final MovieSaveUseCase movieSaveUseCase;
+    private final MovieFindAllUseCase movieFindAllUseCase;
+
+    private final MovieRatingMapping mapping;
+
+    @GetMapping("findmovie")
+    public List<MovieRatingDto> findAllMovie(){
+        List<MovieRating> findall = movieFindAllUseCase.execute();
+        return findall.stream()
+                .map(mapping::toDto)
+                .toList();
+    }
+
+    @PostMapping("moviesave")
+    public MovieRatingDto saveMovie(@RequestBody MovieRatingDto movie){
+        MovieRating save = movieSaveUseCase.execute(mapping.toMovie(movie));
+        return mapping.toDto(save);
+    }
+
 }
