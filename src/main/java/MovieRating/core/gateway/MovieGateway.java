c502be15cb3dd49b50b13cb9806e1d8e5b7c3d38
Alex Bruno <Bssallex@gmail.com>
2025-05-26 15:19:43 -0300

    feat: Implementa o método 'alterar filme por id' e faz conexão com a parte externa da aplicação

--- src/main/java/MovieRating/core/gateway/MovieGateway.java
+++ src/main/java/MovieRating/core/gateway/MovieGateway.java
@@ -3,9 +3,11 @@ package MovieRating.core.gateway;
 import MovieRating.core.entity.MovieRating;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MovieGateway{
 
     MovieRating saveMovie(MovieRating movieRating);
     List<MovieRating> findAllMovies();
+    Optional<MovieRating> findMovieById(Long id, MovieRating rating);
 }
