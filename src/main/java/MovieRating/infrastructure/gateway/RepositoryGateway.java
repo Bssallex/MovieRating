820c10ac8fe1830292135fe89daf72ee9685c5c6
Alex Bruno <Bssallex@gmail.com>
2025-05-26 14:17:20 -0300

    feat: Implementa o JPA nos métodos listar e salvar

--- src/main/java/MovieRating/infrastructure/gateway/RepositoryGateway.java
+++ src/main/java/MovieRating/infrastructure/gateway/RepositoryGateway.java
@@ -1,6 +1,34 @@
 package MovieRating.infrastructure.gateway;
 
+import MovieRating.core.entity.MovieRating;
 import MovieRating.core.gateway.MovieGateway;
+import MovieRating.infrastructure.mapper.MovieEntityMapping;
+import MovieRating.infrastructure.persistence.MovieRatingEntity;
+import MovieRating.infrastructure.persistence.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class RepositoryGateway implements MovieGateway {
+
+    private final MovieEntityMapping mapping;
+    private final MovieRepository repository;
+
+    @Override
+    public List<MovieRating> findAllMovies() {
+        List<MovieRatingEntity> findAll = repository.findAll();
+        return findAll.stream()
+                .map(mapping::toMovie)
+                .toList();
+    }
+
+    @Override
+    public MovieRating saveMovie(MovieRating movieRating) {
+        MovieRatingEntity save = repository.save(mapping.toEntity(movieRating));
+        return mapping.toMovie(save);
+    }
+
 }
