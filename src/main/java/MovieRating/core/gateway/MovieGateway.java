2a023b758c528b94114d833744f3a02cc693b69f
Alex Bruno <Bssallex@gmail.com>
2025-05-26 14:14:45 -0300

    feat: Recebe os métodos listar e salvar e faz a conexão com a parte externa da aplicação

--- src/main/java/MovieRating/core/gateway/MovieGateway.java
+++ src/main/java/MovieRating/core/gateway/MovieGateway.java
@@ -1,4 +1,11 @@
 package MovieRating.core.gateway;
 
-public interface MovieGateway {
+import MovieRating.core.entity.MovieRating;
+
+import java.util.List;
+
+public interface MovieGateway{
+
+    MovieRating saveMovie(MovieRating movieRating);
+    List<MovieRating> findAllMovies();
 }
